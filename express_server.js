d030d97c9cc7a96980d7753c7adf61ee05022161
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-23 13:44:04 -0400

    redirect after form submission

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; // default port 8080
 
 
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: true }));
 
 
 // eslint-disable-next-line func-style
@@ -22,7 +22,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -45,8 +45,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longURL = req.body;
+  let rendId = generateRandomString();
+  urlDatabase[rendId] = longURL['longURL'];
+  res.redirect(`urls/${rendId}`); // update the redirection URL
 });
 
 app.listen(PORT, () => {
