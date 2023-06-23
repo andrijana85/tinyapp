ee0a6758e05b977e6b1fd0b9b9b374b94424fb28
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-23 12:07:20 -0400

    add the body-parser library

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
