06c1d1b48d9ab543d1a3e64e600a40e60475789d
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-23 12:15:04 -0400

    add a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -31,6 +31,10 @@ app.get("/urls/:id", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
