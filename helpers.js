2bf67f921bdc8f72e0c0df6ea03dd948e6e6e77b
andrijana85 <topalovic.andrijana@yahoo.com>
2023-07-01 15:24:21 -0400

    mistake in typing, user instead of users

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ const getUserByEmail = function(email, users) {
   let user;
   for (const id in users) {
     if (users[id].email === email) {
-      user = user[id];
+      user = users[id];
     }
   }
   return user;
